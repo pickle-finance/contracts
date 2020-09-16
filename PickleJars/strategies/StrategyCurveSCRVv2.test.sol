430bd0d94563ff803077262e458f5bd4467b534a
0xpenguin <70997128+0xpenguin@users.noreply.github.com>
2020-09-17 07:20:06 +1000

    Update StrategyCurveSCRVv2.test.sol

--- PickleJars/strategies/StrategyCurveSCRVv2.test.sol
+++ PickleJars/strategies/StrategyCurveSCRVv2.test.sol
@@ -28,6 +28,33 @@ contract DSTestApprox is DSTest {
     }
 }
 
+contract User {
+    function execute(
+        address target,
+        uint256 value,
+        string memory signature,
+        bytes memory data
+    ) public payable returns (bytes memory) {
+        bytes memory callData;
+
+        if (bytes(signature).length == 0) {
+            callData = data;
+        } else {
+            callData = abi.encodePacked(
+                bytes4(keccak256(bytes(signature))),
+                data
+            );
+        }
+
+        (bool success, bytes memory returnData) = target.call{value: value}(
+            callData
+        );
+        require(success, "!user-execute");
+
+        return returnData;
+    }
+}
+
 contract StrategyCurveSCRVv2Test is DSTestApprox {
     using SafeERC20 for IERC20;
     using SafeMath for uint256;
